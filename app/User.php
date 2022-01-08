c71f9195288a91fd2839c097f7ba9962111a937c
asabbir47 <asabbir47@gmail.com>
2022-01-09 01:30:21 +0600

    Update User.php

--- app/User.php
+++ app/User.php
@@ -37,6 +37,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(
+
+        );
+    }
+
     public function posts()
     {
         // return $this->hasMany(Post::class);
